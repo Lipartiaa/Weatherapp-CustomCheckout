4ba1f0bd52f5fed269a30b64198d4a88575e5934
Lipartiaa <nika.lipartia@developers-alliance.com>
2025-01-03 14:42:39 +0400

    Ui Components

--- app/code/Macademy/FreeShippingPromo/view/frontend/web/js/free-shipping-banner.js
+++ app/code/Macademy/FreeShippingPromo/view/frontend/web/js/free-shipping-banner.js
@@ -1,19 +1,61 @@
-
 define([
-    'uiComponent'
+    'uiComponent',
+    'Magento_Customer/js/customer-data',
+    'underscore',
+    'knockout'
 ], function(
-    Component
+    Component,
+    customerData,
+    _,
+    ko
 ) {
     'use strict';
 
     return Component.extend({
         defaults: {
-            template: 'Macademy_FreeShippingPromo/free-shipping-banner'
+            freeShippingThreshold: 100,
+            subtotal: 0.00,
+            template: 'Macademy_FreeShippingPromo/free-shipping-banner',
+            tracks: {
+                subtotal: true
+            }
         },
         initialize: function() {
             this._super();
 
-            console.log(this.message);
+            var self = this;
+            var cart = customerData.get('cart');
+
+            customerData.getInitCustomerData().done(function() {
+                if (!_.isEmpty(cart()) && !_.isUndefined(cart().subtotalAmount)) {
+                    self.subtotal = parseFloat(cart().subtotalAmount);
+                }
+            });
+
+            cart.subscribe(function(cart) {
+                if (!_.isEmpty(cart) && !_.isUndefined(cart.subtotalAmount)) {
+                    self.subtotal = parseFloat(cart.subtotalAmount);
+                }
+            });
+
+            self.message = ko.computed(function() {
+                if (_.isUndefined(self.subtotal) || self.subtotal === 0) {
+                    return self.messageDefault;
+                }
+
+                if (self.subtotal > 0 && self.subtotal < self.freeShippingThreshold) {
+                    var subtotalRemaining = self.freeShippingThreshold - self.subtotal;
+                    var formattedSubtotalRemaining = self.formatCurrency(subtotalRemaining);
+                    return self.messageItemsInCart.replace('$XX.XX', formattedSubtotalRemaining);
+                }
+
+                if (self.subtotal >= self.freeShippingThreshold) {
+                    return self.messageFreeShipping;
+                }
+            });
+        },
+        formatCurrency: function(value) {
+            return '$' + value.toFixed(2);
         }
     });
 });
